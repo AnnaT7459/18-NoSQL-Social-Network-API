c6b344dafc3a8b14133fd680c1ddd76756838e5b
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-04 17:02:53 -0600

    user model logic including email verification and friend count virtual

---
+++ models/Users.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+// email validation - used from module 17 challenge
+const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+// schema to create user model
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // reference: https://mongoosejs.com/docs/validation.html
+        validate: {
+            validator: (value) => emailRegex.test(value),
+            message: 'Email validation failed'
+        }
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
+})
+
+// virtual property that gets the amount of friends per user
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+// reference: https://mongoosejs.com/docs/validation.html
+// db-
+const User = model('User', userSchema);
+const user = new User();
+
+user.email = 'test@test.co';
+user.username = 'test';
+
+let error;
+try {
+  await user.validate();
+} catch (err) {
+  error = err;
+}
+assert.ok(error);
+assert.equal(error.errors['username'].message, 'Oops!');
+assert.equal(error.errors['email'].message, 'Email validation failed');
+
+module.exports = User;
